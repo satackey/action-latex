d0a027c8ca8b3b957f5a4bf32ce3c9988d1936b2
satackey <21271711+satackey@users.noreply.github.com>
2019-12-27 21:40:34 +0900

    workdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ docker pull $BUILD_IMAGE
 
 BUILD_FILE_CMD="sh -c 'TEX_PATH=\"{}\"; cd $(dirname {}); latexmk $LATEXMK_OPTIONS \"\$TEX_PATH\"'"
 
-docker run -v ${PWD}:$WORKDIR --entrypoint '' $BUILD_IMAGE sh -c "echo $BUILD_FILES | xargs -I{} -t $BUILD_FILE_CMD"
+docker run -v ${PWD}:"$WORKDIR" --workdir="$WORKDIR" --entrypoint '' $BUILD_IMAGE sh -c "echo $BUILD_FILES | xargs -I{} -t $BUILD_FILE_CMD"
