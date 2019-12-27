d82c9aadda71346be0d395ae792e5cbebedfdff8
satackey <21271711+satackey@users.noreply.github.com>
2019-12-27 22:32:41 +0900

    修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-docker ps
-
 docker pull $BUILD_IMAGE
 
-BUILD_FILE_CMD="sh -c 'TEX_PATH=\"{}\"; cd $(dirname {}); latexmk $LATEXMK_OPTIONS \"\$TEX_PATH\"'"
+BUILD_FILE_CMD="latexmk $LATEXMK_OPTIONS {}"
 
-docker run -v ${PWD}:"$WORKDIR" --workdir="$WORKDIR" --entrypoint '' $BUILD_IMAGE sh -c "echo $BUILD_FILES | xargs -I{} -t $BUILD_FILE_CMD"
+docker run -v $HOST_WORKSPACE:"$WORKDIR" --workdir="$WORKDIR" --entrypoint '' $BUILD_IMAGE sh -c "echo '$BUILD_FILES' | xargs -I{} -t $BUILD_FILE_CMD"
