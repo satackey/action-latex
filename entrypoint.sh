937782d3ca468b14335b8c5d82a6ce987ae63a01
satackey <21271711+satackey@users.noreply.github.com>
2019-12-27 23:02:56 +0900

    デフォルト値を削除

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,10 @@
 docker pull $BUILD_IMAGE
 
 BUILD_FILE_CMD="latexmk $LATEXMK_OPTIONS {}"
+
+DEFAULT_WORKSPACE="$RUNNER_WORKSPACE/$(basename $RUNNER_WORKSPACE)"
+HOST_WORKSPACE=${HOST_WORKSPACE:-$DEFAULT_WORKSPACE}
+
 env
-docker run -v "$HOST_WORKSPACE":"$WORKDIR" --workdir="$WORKDIR" --entrypoint '' $BUILD_IMAGE sh -c "pwd; ls;echo '$BUILD_FILES' | xargs -I{} -t $BUILD_FILE_CMD"
+
+docker run -v "$BUILD_DIR":"$WORKDIR" --workdir="$WORKDIR" --entrypoint '' $BUILD_IMAGE sh -c "pwd; ls;echo '$BUILD_FILES' | xargs -I{} -t $BUILD_FILE_CMD"
