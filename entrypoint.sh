7489219a0b175d65c5674c9b18a2cd20733bd86b
satackey <21271711+satackey@users.noreply.github.com>
2019-12-27 22:50:15 +0900

    pwd ls in container

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ docker pull $BUILD_IMAGE
 BUILD_FILE_CMD="latexmk $LATEXMK_OPTIONS {}"
 pwd
 ls
-docker run -v "$HOST_WORKSPACE":"$WORKDIR" --workdir="$WORKDIR" --entrypoint '' $BUILD_IMAGE sh -c "echo '$BUILD_FILES' | xargs -I{} -t $BUILD_FILE_CMD"
+docker run -v "$HOST_WORKSPACE":"$WORKDIR" --workdir="$WORKDIR" --entrypoint '' $BUILD_IMAGE sh -c "pwd; ls;echo '$BUILD_FILES' | xargs -I{} -t $BUILD_FILE_CMD"
