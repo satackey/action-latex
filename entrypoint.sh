668fe9bc13f7fe3967425f461bdf80778f0e5f9f
satackey <21271711+satackey@users.noreply.github.com>
2019-12-27 21:37:49 +0900

    escape

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ docker ps
 
 docker pull $BUILD_IMAGE
 
-BUILD_FILE_CMD="sh -c 'TEX_PATH=\"{}\"; cd $(dirname {}); latexmk $LATEXMK_OPTIONS \"$TEX_PATH\"'"
+BUILD_FILE_CMD="sh -c 'TEX_PATH=\"{}\"; cd $(dirname {}); latexmk $LATEXMK_OPTIONS \"\$TEX_PATH\"'"
 
 docker run -v ${PWD}:$WORKDIR --entrypoint '' $BUILD_IMAGE sh -c "echo $BUILD_FILES | xargs -I{} -t $BUILD_FILE_CMD"
