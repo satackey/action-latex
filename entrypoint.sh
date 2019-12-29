8f713fa85325337f52795b21a2fa5a05d388b108
satackey <21271711+satackey@users.noreply.github.com>
2019-12-29 12:40:43 +0900

    修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,14 @@ docker pull $BUILD_IMAGE
 
 BUILD_FILE_CMD="latexmk $LATEXMK_OPTIONS {}"
 
-export DEFAULT_WORKSPACE="$RUNNER_WORKSPACE/$(basename $RUNNER_WORKSPACE)"
-export HOST_WORKSPACE=${HOST_WORKSPACE:-$DEFAULT_WORKSPACE}
+DEFAULT_WORKSPACE="$RUNNER_WORKSPACE/$(basename $RUNNER_WORKSPACE)"
+HOST_WORKSPACE=${HOST_WORKSPACE:-$DEFAULT_WORKSPACE}
 
-env
-echo hogehoge
-
-docker run -v "$HOST_WORKSPACE":"$WORKDIR" --workdir="$WORKDIR" --entrypoint '' $BUILD_IMAGE sh -c "pwd; ls;echo '$BUILD_FILES' | xargs -I{} -t $BUILD_FILE_CMD"
+docker run \
+    -v "$HOST_WORKSPACE":"$WORKDIR" \
+    --workdir="$WORKDIR" \
+    --entrypoint '' \
+    $BUILD_IMAGE \
+    sh -c "
+        echo \"$BUILD_FILES\" | xargs -I{} -t $BUILD_FILE_CMD
+    "
