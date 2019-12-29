087087da1d62d82d2d31a8924b011bdccbfe7883
satackey <21271711+satackey@users.noreply.github.com>
2019-12-29 13:06:15 +0900

    ファイルの場所へcdする

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 docker pull $BUILD_IMAGE
 
-BUILD_FILE_CMD="latexmk $LATEXMK_OPTIONS {}"
+BUILD_FILE_CMD="sh -c '
+    TEX_FILE={}
+    cd $(dirname \$TEX_FILE)
+    latexmk $LATEXMK_OPTIONS $(basename \$TEX_FILE)
+'"
 
 DEFAULT_WORKSPACE="$RUNNER_WORKSPACE/$(basename $RUNNER_WORKSPACE)"
 HOST_WORKSPACE=${HOST_WORKSPACE:-$DEFAULT_WORKSPACE}
